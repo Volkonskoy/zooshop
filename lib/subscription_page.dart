7f8c595435df18ef9e547e207d919ca205081223
NureBurkhanInzhyla <inzhyla.burkhan@nure.ua>
2025-05-23 12:39:14 +0200

    Update subscription_page.dart imports

--- lib/subscription_page.dart
+++ lib/subscription_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/account_layout.dart';
+import 'package:zooshop/account_layout.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/gestures.dart';
